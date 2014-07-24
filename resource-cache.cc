eb94f3eabac40ff2b652d0aa1405d9e5113afe40
Mostafa Razavi <mostafa@sepent.com>
2014-07-24 12:33:11 +0430

    Added downscale_image method from SOIL. Buggy at the moment.

--- resource-cache.cc
+++ resource-cache.cc
@@ -203,6 +203,77 @@ Mix_Chunk *GetSound(const string &name) {
   return chunk;
 }
 
+int
+        downscale_image
+        (
+                const unsigned char* const orig,
+                int width, int height, int channels,
+                unsigned char* resampled,
+                int block_size_x, int block_size_y,
+                int new_width, int new_height
+        )
+{
+        int mip_width, mip_height;
+        int i, j, c;
+
+        /*	error check	*/
+        if( (width < 1) || (height < 1) ||
+                (channels < 1) || (orig == NULL) ||
+                (resampled == NULL) ||
+                (block_size_x < 1) || (block_size_y < 1) )
+        {
+                /*	nothing to do	*/
+                return 0;
+        }
+        mip_width = new_width;// width / block_size_x;
+        mip_height = new_height;// height / block_size_y;
+        /*if( mip_width < 1 )
+        {
+                mip_width = 1;
+        }
+        if( mip_height < 1 )
+        {
+                mip_height = 1;
+                }*/
+        for( j = 0; j < mip_height; ++j )
+        {
+                for( i = 0; i < mip_width; ++i )
+                {
+                        for( c = 0; c < channels; ++c )
+                        {
+                                const int index = (j*block_size_y)*width*channels + (i*block_size_x)*channels + c;
+                                int sum_value;
+                                int u,v;
+                                int u_block = block_size_x;
+                                int v_block = block_size_y;
+                                int block_area;
+                                /*	do a bit of checking so we don't over-run the boundaries
+                                        (necessary for non-square textures!)	*/
+                                if( block_size_x * (i+1) > width )
+                                {
+                                        u_block = width - i*block_size_y;
+                                }
+                                if( block_size_y * (j+1) > height )
+                                {
+                                        v_block = height - j*block_size_y;
+                                }
+                                block_area = u_block*v_block;
+                                /*	for this pixel, see what the average
+                                        of all the values in the block are.
+                                        note: start the sum at the rounding value, not at 0	*/
+                                sum_value = block_area >> 1;
+                                for( v = 0; v < v_block; ++v )
+                                for( u = 0; u < u_block; ++u )
+                                {
+                                        sum_value += orig[index + v*width*channels + u*channels];
+                                }
+                                resampled[j*mip_width*channels + i*channels + c] = sum_value / block_area;
+                        }
+                }
+        }
+        return 1;
+}
+
 GLuint GetTexture(const string &name, const string &type) {
   auto it = texture_cache.find(name);
   if (it != texture_cache.end())
@@ -217,6 +288,35 @@ GLuint GetTexture(const string &name, const string &type) {
     throw runtime_error(ss.str());
   }
   cout << w << " " << h << " " << channels << " " << GL_MAX_TEXTURE_SIZE << endl;
+
+  int nw = w;
+  int nh = h;
+  if (w > GL_MAX_TEXTURE_SIZE) {
+    float ratio = (float) h / w;
+    nw = GL_MAX_TEXTURE_SIZE;
+    nh = (float) nw * ratio;
+    cout << "resize1: ratio=" << ratio << " nw=" << nw << " nh=" << nh << endl;
+  }
+
+  if (h > GL_MAX_TEXTURE_SIZE) {
+    float ratio = (float) w / h;
+    nh = GL_MAX_TEXTURE_SIZE;
+    nw = (float) nh * ratio;
+    cout << "resize2: ratio=" << ratio << " nw=" << nw << " nh=" << nh << endl;
+  }
+
+  if (nw != w || nh != h) {
+    cout << "[" << name << "] obtaining: " << nw * nh * channels << " (nw=" << nw << ", nh=" << nh << ", ch=" << channels << ")" << endl;
+    uint8_t *resampled = new uint8_t[nw * nh * channels];
+    int bx = w / nw;
+    int by = h / nh;
+    cout << "bx=" << bx << " by=" << by << endl;
+    downscale_image(img, w, h, channels, resampled, bx, by, nw, nh);
+    stbi_image_free(img);
+    img = resampled;
+    w = nw;
+    h = nh;
+  }
 
   GLuint texture;
   glGenTextures(1, &texture);
