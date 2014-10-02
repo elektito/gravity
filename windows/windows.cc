4740f99535810ddb7848b3d4ce10ca9b89f54ccc
Mostafa Razavi <mostafa@sepent.com>
2014-10-02 20:43:34 +0330

    The save file is saved in LocalAppData directory in Windows.

--- windows/windows.cc
+++ windows/windows.cc
@@ -1,7 +1,17 @@
 #include <string>
+#include <sstream>
+#include <iostream>
+
+#include <windows.h>
+#include <shlobj.h>
 
 using namespace std;
 
 string GetUserHomeDirectory() {
-  return ".";
+  char localAppData[MAX_PATH];
+
+  if (SUCCEEDED(SHGetFolderPath(NULL, CSIDL_LOCAL_APPDATA, NULL, 0, localAppData)))
+    return string(localAppData);
+  else
+    return ".";
 }
